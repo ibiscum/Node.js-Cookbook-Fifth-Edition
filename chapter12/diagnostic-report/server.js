e571be41ecad1c720ba36612db291b9690a1fe54
ibiscum <ulf.luecking@gmail.com>
2025-12-03 13:29:22 +0100

    linting

--- chapter12/diagnostic-report/server.js
+++ chapter12/diagnostic-report/server.js
@@ -4,4 +4,4 @@ const path = require('node:path');
 process.report.directory = path.join(__dirname, 'reports');
 process.report.filename = 'my-diagnostic-report.json';
 
-http.get('hello://localhost:3000', (response) => {});
+http.get('hello://localhost:3000', () => {});
